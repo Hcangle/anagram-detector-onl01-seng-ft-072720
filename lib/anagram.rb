93c966b3344e6b8ed0e34dc9879956aa9385eb09
Hannah Angle <hannahcangle@gmail.com>
2020-08-08 16:31:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,8 @@ def match(some_words)
   puts "Must find #{self.word} inside the following string: #{some_words}"
   some_words.find_all do |word|
   if word.split("").sort == self.some_words.split("").sort 
-    
+  word
+end 
+
 
 end
